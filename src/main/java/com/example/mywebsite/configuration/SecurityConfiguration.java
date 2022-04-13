227a6169b2164c703b1cf7c589c71609fd137e05
HONG <kingtjrals@naver.com>
2022-04-13 19:01:55 +0900

    4/13 user init
    Signed-off-by: HONG <kingtjrals@naver.com>

---
+++ src/main/java/com/example/mywebsite/configuration/SecurityConfiguration.java
@@ -0,0 +1,87 @@
+package com.example.mywebsite.configuration;
+
+import com.example.mywebsite.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static java.rmi.server.RMISocketFactory.getFailureHandler;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserAuthenticationFailureHandler getFailureHandler(){
+        return new UserAuthenticationFailureHandler();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/favicon.ico", "/files/**");
+        web.ignoring().antMatchers("/assets/**");
+        web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/images/**");
+        web.ignoring().antMatchers("/js/**");
+
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.headers().frameOptions().sameOrigin(); // 에디터 권한 이슈 해결
+
+        //로그인 없이 접근 권한 가능 페이지 설정
+        http.authorizeRequests()
+                .antMatchers("/"
+                        , "/user/register"
+                        , "/user/email_auth"
+                        , "/user/find/password"
+                        , "/user/reset/password"
+                        )
+                        .permitAll();
+
+
+        http.formLogin()
+                .loginPage("/user/login")
+                .failureHandler(getFailureHandler()) // 로그인 실패시
+                .permitAll();
+
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true);//로그아웃 하면 세션 초기화
+
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(userService)
+                .passwordEncoder(getPasswordEncoder());
+
+        super.configure(auth);
+    }
+
+
+}
---
+++ src/main/java/com/example/mywebsite/user/entity/User.java
@@ -0,0 +1,44 @@
+package com.example.mywebsite.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+    @Id
+    private String userId;
+
+    private String userName;
+    private String phone;
+    private String password;
+    private LocalDateTime regDt;
+    private LocalDateTime udtDt;//회원정보 수정일
+
+    private boolean emailAuthYn;
+    private LocalDateTime emailAuthDt;
+    private String emailAuthKey;
+
+    private String resetPasswordKey;
+    private LocalDateTime resetPasswordLimitDt;
+
+    private boolean adminYn;
+
+    private String userStatus;//이용가능, 정지
+
+    // 우편번호 api를 위한 칼럼
+    private String zipcode;
+    private String addr;
+    private String addrDetail;
+}
